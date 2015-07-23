2b12e34366444d7c9f4064457ced1bcb7b0a6748
Serge45 <Serge45497@gmail.com>
2015-07-23 20:19:49 +0800

    Added appreciate routing function for post, added templates for review post.

--- bookreview.py
+++ bookreview.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from flask import Flask, render_template
+from flask import Flask, render_template, abort
 from flask_admin import Admin
 from flask.ext import login
 from flask.ext.sqlalchemy import SQLAlchemy
@@ -10,7 +10,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 app.config['SECRET_KEY'] = 'sdghir035nvawihegovnv0-64y jioj'
 
 db = SQLAlchemy(app)
-from model import User, BookReview
+from model import User, BookReview, Book
 
 def init_login():
     login_manager = login.LoginManager()
@@ -25,8 +25,27 @@ def index():
     reviews = db.session.query(BookReview).all()
     return render_template("index.html", reviews=reviews)
 
-@app.route('/<user>/<slug>/'):
-    pass
+@app.route('/<int:user_id>/')
+def reviews(user_id):
+    user = db.session.query(User).get(user_id)
+    if user:
+        book_reviews = []
+        for r in user.reviews:
+            book_reviews.append((db.session.query(Book).get(r.book_id)))
+        return render_template('query_result.html', result=book_reviews,
+            username=user.username, user_id=user_id)
+    return abort(404)
+
+@app.route('/<int:user_id>/<int:slug_id>/')
+def review(user_id, slug_id):
+    user = db.session.query(User).get(user_id)
+    if user:
+        review = db.session.query(BookReview).get(slug_id)
+        book = db.session.query(Book).get(review.book_id)
+        return render_template('post.html', title=book.title, 
+            review=review)
+    return abort(404)
+
 
 from modelview import BackendAdminIndexView
 
