1ba1dbe5027657f40cd13068be103799c3af34ec
Serge45 <Serge45497@gmail.com>
2015-07-27 18:17:40 +0800

    Modified resources attaching for more RESTful.

--- bookreview.py
+++ bookreview.py
@@ -25,25 +25,30 @@ def index():
     reviews = db.session.query(BookReview).all()
     return render_template("index.html", reviews=reviews)
 
-@app.route('/<int:user_id>/')
-def reviews(user_id):
-    user = db.session.query(User).get(user_id)
+@app.route('/<username>/')
+def reviews(username):
+    user = db.session.query(User).filter_by(username=username).first()
     if user:
         book_reviews = []
+
         for r in user.reviews:
-            book_reviews.append((db.session.query(Book).get(r.book_id)))
+            book_reviews.append(((db.session.query(Book).get(r.book_id)), r.slug))
         return render_template('query_result.html', result=book_reviews,
-            username=user.username, user_id=user_id)
+            username=user.username)
     return abort(404)
 
-@app.route('/<int:user_id>/<int:slug_id>/')
-def review(user_id, slug_id):
-    user = db.session.query(User).get(user_id)
+@app.route('/<username>/<slug>/')
+def review(username, slug):
+    user = db.session.query(User).filter_by(username=username).first()
     if user:
-        review = db.session.query(BookReview).get(slug_id)
-        book = db.session.query(Book).get(review.book_id)
-        return render_template('post.html', title=book.title, 
-            review=review)
+        review = db.session.query(BookReview).filter_by(slug=slug).first()
+
+        if review:
+            book = db.session.query(Book).get(review.book_id)
+
+            if book:
+                return render_template('post.html', title=book.title, 
+                    review=review)
     return abort(404)
 
 
